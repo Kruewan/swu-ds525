462461be25453697273f436342df42ca087450eb
Kruewan <kruewan2350@gmail.com>
2022-08-31 10:31:13 +0000

    Fix

--- 02-data-modeling-ii/etl.py
+++ 02-data-modeling-ii/etl.py
@@ -7,6 +7,9 @@ from typing import List
 table_drop_events = "DROP TABLE IF EXISTS events"
 table_drop_payloads = "DROP TABLE IF EXISTS payloads"
 table_drop_actors = "DROP TABLE IF EXISTS actors"
+table_drop_total_events = "DROP TABLE IF EXISTS total_events"
+table_drop_users = "DROP TABLE IF EXISTS users"
+
 
 table_create_events = """
     CREATE TABLE IF NOT EXISTS events
@@ -16,6 +19,7 @@ table_create_events = """
         public text,
         created_at TIMESTAMP,
         PRIMARY KEY (
+            id,
             type
         )
     )
@@ -26,8 +30,10 @@ table_create_actors = """
     (
         id text,
         login text,
+        display_login text,
         PRIMARY KEY (
-            id
+            id,
+            login
         )
     )
 """
@@ -38,20 +44,51 @@ table_create_payloads = """
         id text,
         action text,
         PRIMARY KEY (
-            id
+            id,
+            action
         )
     )
+"""
+
+table_create_total_events = """
+    CREATE TABLE IF NOT EXISTS total_events
+    (
+        type text,
+        actor_login text,
+        actor_display_login text,
+        PRIMARY KEY (
+            actor_login,
+            type
+        )
+    )
+"""
+
+table_create_users = """
+    CREATE TABLE IF NOT EXISTS users
+    (
+        user text,
+        total int ,
+        PRIMARY KEY (
+            user,
+            total
+        )
+    )
+    WITH CLUSTERING ORDER BY (total desc)
 """
 
 create_table_queries = [
     table_create_payloads,
     table_create_actors,
     table_create_events,
+    table_create_total_events,
+    table_create_users,
 ]
 drop_table_queries = [
+    table_drop_users,
     table_drop_events,
     table_drop_payloads,
     table_drop_actors,
+    table_drop_total_events,
 ]
 
 def drop_tables(session):
@@ -99,12 +136,13 @@ def process(session, filepath):
                     # Insert data into tables events
                     query = f"""INSERT INTO events (id, type, public, created_at) VALUES ('{each["id"]}', '{each["type"]}', '{each["public"]}', '{each["created_at"]}')"""
                     session.execute(query)
+                    i = i+1
                 except:
                     pass
 
                 try:
                     # Insert data into tables actors
-                    query = f"""INSERT INTO actors (id, login) VALUES ('{each["id"]}', '{each["actor"]["login"]}')"""
+                    query = f"""INSERT INTO actors (id, login, display_login) VALUES ('{each["id"]}', '{each["actor"]["login"]}', '{each["actor"]["display_login"]}')"""
                     session.execute(query)
                 except:
                     pass
@@ -113,15 +151,47 @@ def process(session, filepath):
                     # Insert data into tables payloads
                     query = f"""INSERT INTO payloads (id, action) VALUES ('{each["id"]}', '{each["payload"]["action"]}')"""
                     session.execute(query)
-                    
-                    i = i+1
                 except:
                     pass
-             
 
+    # print("Data inserted total " + str(i) + " records")
 
-    print("Data inserted total " + str(i) + " records")
+    # Insert data for UI
+    j = 0
+    for datafile in all_files:
+        with open(datafile, "r") as f:
+            data = json.loads(f.read())
+            for each in data:
 
+                try:
+                    # Insert data into tables total_events
+                    query = f"""INSERT INTO total_events (type, actor_login, actor_display_login) VALUES ('{each["type"]}', '{each["actor"]["login"]}', '{each["actor"]["display_login"]}')"""
+                    session.execute(query)
+                    j = j+1
+                except:
+                    pass
+
+    print("Data inserted total " + str(j) + " records")
+
+def insert_data(session):
+
+    query_select = """
+    SELECT actor_login, COUNT (*)  AS user_count  FROM  total_events  GROUP BY  actor_login  -- limit 10
+    """
+    try:
+        rows = session.execute(query_select)
+    except Exception as e:
+        print(e)
+
+    for row in rows:
+
+        try:
+            query_insert = f"""
+            INSERT INTO users (user, total) VALUES ('{row[0]}', {row[1]})
+            """
+            session.execute(query_insert)
+        except Exception as e:
+            print(e)
 
 
 def main():
@@ -150,11 +220,13 @@ def main():
 
     process(session, filepath="../data")
    
+    insert_data(session)
+
     # Select data in Cassandra and print them to stdout
     query = """
-    -- SELECT actor_id, actor_display_login, COUNT (*)  AS user_count  FROM actors  GROUP BY actor_id, actor_display_login
-    select * from actors
+    SELECT  *  from users
     """
+
     try:
         rows = session.execute(query)
     except Exception as e:
