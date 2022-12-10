79deae6318156e345f02741d8ac2551f03994554
Kruewan <kruewan2350@gmail.com>
2022-12-10 10:33:54 +0000

    install boto3

--- 08-capstone-project/dags/etl.py
+++ 08-capstone-project/dags/etl.py
@@ -2,11 +2,38 @@ import json
 import glob
 import os
 import psycopg2
+import boto3
 from typing import List
 from airflow import DAG
 from airflow.utils import timezone
 from airflow.operators.python import PythonOperator
 from airflow.providers.postgres.hooks.postgres import PostgresHook
+
+
+
+def _upload_files():
+
+    aws_access_key_id = ASIA46YTXNWJI2PQSNOC
+    aws_secret_access_key = c7u6/BTOJY1o7HiY3AsOTmHyB4lHk9znZXcqbtt1
+    aws_session_token = FwoGZXIvYXdzEFsaDH5LTm6wdrUJt1/G/yLIAaZGcgQ5NYTAvL1WMK8uQTRH4IYGY75vnVIBzWcnsfC9DS/YO5iIs9qRlWoxsvPn6LfDo7x9RJhHm1sXafKNoSoz8l/v0q1UWN94Ez4IUV3HczuHF/J5bARU7FBw2n7W+zTtMlwj4Grn+mdVHbtYfdyngrEpKgzc1CYKLOrVZB1pvha4nuWe+ycIzOqomZ2DIDfjuq1uK7cGqEne8bQkpuzSE3TwuMY1Zsyq2FO5TSqW8kyOuUWmwe2S76vyxK5BNSxEh4ndyeu1KPyX0ZwGMi3Zol/pNOteafYs1j5H0pJEiP13wgworPVjeMb861eTu/FFDRpUrqB7kzbGHt8=
+
+    client = boto3.client(
+        "s3",
+        aws_access_key_id=aws_access_key_id,
+        aws_secret_access_key=aws_secret_access_key,
+        aws_session_token=aws_session_token
+    )
+    print(client)
+
+    response = client.list_objects(
+        Bucket="zkan-swu-labs",
+        MaxKeys=2,
+    )
+
+    contents = response["Contents"]
+    for content in contents:
+        print(content["Key"], content["Size"])
+
 
 
 def _get_files():
@@ -99,7 +126,6 @@ def _insert_tables():
 
 
 
-
 with DAG(
     "etl",
     start_date=timezone.datetime(2022, 10, 15),
@@ -107,6 +133,11 @@ with DAG(
     tags=["workshop"],
     catchup=False,
 ) as dag:
+
+    upload_files = PythonOperator(
+        task_id="upload_files",
+        python_callable=_upload_files,
+    )
 
     get_files = PythonOperator(
         task_id="get_files",
@@ -128,5 +159,6 @@ with DAG(
         python_callable=_insert_tables,
     )
     
+    upload_files
 
-    drop_tables >> [get_files, create_tables] >> insert_tables
+    #drop_tables >> [get_files, create_tables] >> insert_tables
