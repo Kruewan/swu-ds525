f97a6350905385598b8b2a42f3c73d1d51146e9d
Kruewan <kruewan2350@gmail.com>
2022-12-11 07:56:49 +0000

    Fix

--- 08-capstone-project/redshift_to_dataframe.py
+++ 08-capstone-project/redshift_to_dataframe.py
@@ -1,140 +1,45 @@
-from typing import NewType
+import json
+import glob
+import os
+#import psycopg2
+import boto3
+from typing import List
+from airflow import DAG
+from airflow.utils import timezone
+from airflow.operators.python import PythonOperator
+from airflow.providers.postgres.hooks.postgres import PostgresHook
 
-import psycopg2
+def redshift_to_dataframe(data):
+    df_labels = []
 
+    for i in data['ColumnMetadata']:
+        df_labels.append(i['label'])
 
-PostgresCursor = NewType("PostgresCursor", psycopg2.extensions.cursor)
-PostgresConn = NewType("PostgresConn", psycopg2.extensions.connection)
+    df_data = []
 
-table_drop_events = "DROP TABLE IF EXISTS Event"
-table_drop_actors = "DROP TABLE IF EXISTS Actor"
-table_drop_repos = "DROP TABLE IF EXISTS Repo"
-table_drop_payloads = "DROP TABLE IF EXISTS Payload"
-table_drop_orgs = "DROP TABLE IF EXISTS Org"
+    for i in data['Records']:
+        object_data = []
 
-# Event
-table_create_events = """
-    CREATE TABLE IF NOT EXISTS Event (
-        eventId TEXT NOT NULL,
-        type VARCHAR(255),
-        actorId INT NOT NULL,
-        repoId INT NOT NULL,
-        action VARCHAR(255),
-        public BOOLEAN,
-        created_at TIMESTAMP,
-        orgId INT,
-        PRIMARY KEY (eventId),
-        CONSTRAINT fk_actor FOREIGN KEY (actorId)
-            REFERENCES Actor (actorId),
-        CONSTRAINT fk_repo FOREIGN KEY (repoId)
-            REFERENCES Repo (repoId)
-    )
-"""
+        for j in i:
+            object_data.append(list(j.values())[0])
 
-# Actor
-table_create_actors = """
-    CREATE TABLE IF NOT EXISTS Actor (
-        actorId INT NOT NULL,
-        login VARCHAR(255),
-        display_login VARCHAR(255),
-        gravatar_id VARCHAR(255),
-        url VARCHAR(255),
-        avatar_url VARCHAR(255),
-        PRIMARY KEY (actorId)
-    )
-"""
+        df_data.append(object_data)
 
-# Repo
-table_create_repos = """
-    CREATE TABLE IF NOT EXISTS Repo (
-        repoId INT NOT NULL,
-        name VARCHAR(255),
-        url VARCHAR(255),
-        PRIMARY KEY (repoId)
-    )
-"""
+        df = pd.DataFrame(columns=df_labels, data=df_data)
 
-# Payload
-table_create_payloads = """
-    CREATE TABLE IF NOT EXISTS Payload (
-        push_id INT,
-        action VARCHAR(255),
-        ref VARCHAR(255),
-        author_email VARCHAR(255),
-        author_name VARCHAR(255),
-        message VARCHAR(255),
-        distincts BOOLEAN,
-        url VARCHAR(255)
-    )
-"""
+        df.to_csv('your-file.csv')
 
-# Org
-table_create_orgs = """
-    CREATE TABLE IF NOT EXISTS Org (
-        orgId INT NOT NULL,
-        login VARCHAR(255),
-        gravatar_id VARCHAR(255),
-        url VARCHAR(255),
-        avatar_url VARCHAR(255),
-        PRIMARY KEY (orgId)
-    )
-"""
-
-
-create_table_queries = [
-    table_create_actors,
-    table_create_repos,
-    table_create_payloads,
-    table_create_orgs,
-    table_create_events,
-]
-drop_table_queries = [
-    table_drop_events,
-    table_drop_actors,
-    table_drop_repos,
-    table_drop_payloads,
-    table_drop_orgs,
-]
-
-
-def drop_tables(cur: PostgresCursor, conn: PostgresConn) -> None:
-    """
-    Drops each table using the queries in `drop_table_queries` list.
-    """
-    for query in drop_table_queries:
-        cur.execute(query)
-        conn.commit()
-
-
-def create_tables(cur: PostgresCursor, conn: PostgresConn) -> None:
-    """
-    Creates each table using the queries in `create_table_queries` list.
-    """
-    for query in create_table_queries:
-        cur.execute(query)
-        conn.commit()
 
 
 def main():
-    """
-    - Drops (if exists) and Creates the sparkify database.
-    - Establishes connection with the sparkify database and gets
-    cursor to it.
-    - Drops all the tables.
-    - Creates all tables needed.
-    - Finally, closes the connection.
-    """
-    conn = psycopg2.connect(
-        "host=127.0.0.1 dbname=postgres user=postgres password=postgres"
-    )
+  
+    hook = PostgresHook(postgres_conn_id="my_redshift")
+    conn = hook.get_conn()
     cur = conn.cursor()
 
-    drop_tables(cur, conn)
-    create_tables(cur, conn)
+    redshift_to_dataframe()
 
     conn.close()
-
-    print("Table created")
 
 
 if __name__ == "__main__":
